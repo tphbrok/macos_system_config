275f5f9ed4745d164b3ee4560f4ed18aa1ef6b8a
Thomas Brok <tphbrok@gmail.com>
2023-10-04 21:20:28 +0200

    Add Homebrew update when already installed

--- scripts/install-brew.sh
+++ scripts/install-brew.sh
@@ -4,4 +4,7 @@ if [ -z $(which brew) ]
 then
     echo -e "> Installing Homebrew..."
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+else
+    echo -e "> Updating Homebrew..."
+    brew update
 fi
